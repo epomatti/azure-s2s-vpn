22f82080dfa7bf14789fb72a83129084c7e41f66
Evandro Pomatti <evandro.pomatti@gmail.com>
2025-03-20 14:15:49 -0300

    feat: removed public ip from the host server on aws

--- aws/modules/ec2/server/main.tf
+++ aws/modules/ec2/server/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "default" {
   ami           = var.ami
   instance_type = var.instance_type
 
-  associate_public_ip_address = true
+  associate_public_ip_address = false
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [aws_security_group.default.id]
 
